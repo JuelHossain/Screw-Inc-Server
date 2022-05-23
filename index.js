e6c3ed4ab77d2454c68c77c0e8b9260bbdf13038
juelhossain <juelhossainnn@gmail.com>
2022-05-23 21:47:00 +0600

    upsert users successfully

--- index.js
+++ index.js
@@ -39,20 +39,34 @@ const run = async () => {
   try {
     await client.connect();
     console.log('Mongo Connected');
-    const users = client.db('Screw').collection('users');
+    const usersCollection = client.db('Screw').collection('users');
     //auth
-    app.post('/login', async (req, res) => {
+    // giving user a access token when login
+    app.put('/users/:email', async (req, res) => {
+      const email = req.params.email;
+      const filter = { email: email };
+      const options = { upsert: true };
       const user = req.body;
-      const result = await users.insertOne(user);
-      console.log(user.displayName, 'Added Successfully to the user list');
-      const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
-        expiresIn: '1h',
+      updatedUser = {
+        $set: user,
+      };
+      const result = await usersCollection.updateOne(filter, updatedUser, options);
+      const token = jwt.sign({email}, process.env.ACCESS_TOKEN_SECRET, {
+        expiresIn: "1h",
       });
-      res.send({ accessToken,result });
+      res.send({ token, result });
+      console.log(user.displayName,'posted')
+    });
+
+    app.get('/users', verifyJwt, async (req, res) => {
+      const query = {};
+      const users = await usersCollection.find(query).toArray();
+      res.send(users);
+      console.log(users.length, 'user sent');
     })
     
   } finally {
-    console.log("Everything is fine.");
+    console.log("!");
   }
 };
 run().catch(console.dir);
